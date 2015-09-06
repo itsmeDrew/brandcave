d9efe1001ec22012adcecb805b867bf6dd468f2a
Drew Roberts <drew.roberts@methodfactory.com>
2015-09-05 22:55:51 -0400

    Finish building about page.

--- htdocs/content/themes/brandcave/templates/about.php
+++ htdocs/content/themes/brandcave/templates/about.php
@@ -39,11 +39,11 @@ get_header();
     </div><!--  end navbar -->
 </div> <!-- end menu-dropdown -->
 <div class="row about-row">
-    <div class="col-md-6 about-block about-block--large about-block__cody">
-        <div class="about-block-inner"></div>
+    <div class="col-md-6 about__block about__block--large about__block__cody">
+        <div class="about__block-inner"></div>
     </div>
-    <div class="col-md-6 about-block about-block--large block--white">
-        <div class="about-block-inner">
+    <div class="col-md-6 about__block about__block--large block--white">
+        <div class="about__block-inner">
             <h2>cody miles <span class="about-title">| creative director</span></h2>
             <p>A strong conceptual thinker and hands-on leader, Cody Miles is an Austin-based creative director and co-founder of Brandcave. Responsible for overall vision and strategy, Cody serves as liaison between design and production.</p>
             <p>A small agency with strong roots in inbound marketing and web design, Cody co-launched Brandcave in September 2015 as a complete resource for web, social video and print. In it’s short life, Brandcave has grown to over 20 national clients.</p>
@@ -51,27 +51,62 @@ get_header();
             <p>Cody studied public relations at Sam Houston State University.</p>
         </div>
     </div>
+    <ul class="about__btn--list">
+        <li class="about__btn--item">
+            <img class="about__btn--img" src="<?php bloginfo(template_url); ?>/img/about-btn-1.png" alt="about-btn-1">
+        </li>
+        <li class="about__btn--item">
+            <img class="about__btn--img" src="<?php bloginfo(template_url); ?>/img/about-btn-2.png" alt="about-btn-2">
+        </li>
+        <li class="about__btn--item">
+            <img class="about__btn--img" src="<?php bloginfo(template_url); ?>/img/about-btn-3.png" alt="about-btn-3">
+        </li>
+        <li class="about__btn--item">
+            <img class="about__btn--img" src="<?php bloginfo(template_url); ?>/img/about-btn-4.png" alt="about-btn-4">
+        </li>
+    </ul>
 </div>
 <div class="row about-row">
-    <div class="col-md-6 about-block about-block--small block--blue">
-        <div class="about-block-inner">
+    <div class="col-md-6 about__block about__block--small block--blue">
+        <div class="about__block-inner">
             <h2>we're built on data</h2>
         </div>
     </div>
-    <div class="col-md-6 about-block about-block--small block--white">
-        <div class="about-block-inner">
+    <div class="col-md-6 about__block about__block--small block--white">
+        <div class="about__block-inner">
             <p>Data and creativity live harmoniously in the cave. In a word, we love analytics. We use market research and customer data to maximize ROI so our clients get the most powerful and lead generating solutions for their money.</p>
         </div>
     </div>
-    <div class="col-md-6 about-block about-block--small block--white">
-        <div class="about-block-inner">
+    <div class="col-md-6 about__block about__block--small block--white">
+        <div class="about__block-inner">
             <p>Every good storyteller has a deep-rooted interest in the protagonist. We do too. So, we invest in our client relationships in order to share deeper, more impactful messages. History, passions and vision. All included.</p>
         </div>
     </div>
-    <div class="col-md-6 about-block about-block--small block--blue">
-        <div class="about-block-inner">
+    <div class="col-md-6 about__block about__block--small block--blue">
+        <div class="about__block-inner">
             <h2>We’re  Our Client’s Storytellers</h2>
         </div>
+    </div>
+    <div class="col-md-6 about__block about__block--small block--blue">
+        <div class="about__block-inner">
+            <h2>We’re lean & agile evangelists</h2>
+        </div>
+    </div>
+    <div class="col-md-6 about__block about__block--small block--white">
+        <div class="about__block-inner">
+            <p>We haven’t been in the business for 30 years and that’s a good thing. At Brandcave, we stay agile by adapting to changing marketing conditions, reacting to new data, and constantly improving. There’s no starch in our pants.</p>
+        </div>
+    </div>
+</div>
+<div class="row about__cta--wrapper">
+    <div class="col-md-5">
+       <h3>lets create</h3>
+       <h1>something</h1>
+       <h1>great.</h1>
+    </div>
+    <div class="col-md-5 col-md-push-1">
+        <p>At Brandcave, we don’t sell our time. We sell intellectual capital and business results. We’re able to offer integrated digital marketing services to meet our client’s specific needs. Rather than focusing on overhead, hours and markup, we’re able to focus on value and effectiveness. At the end of the day, our relationships are profitable because our work is strategic and we deliver every project like it was our last.</p>
+        <h4><a href="#">get started</a></h4>
     </div>
 </div>
 </div>
