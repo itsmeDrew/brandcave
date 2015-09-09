3b52c1810aab82fe1d6693b252d5870a115282c0
Drew Roberts <drew.roberts@methodfactory.com>
2015-09-08 22:22:36 -0400

    Fixed nav

--- htdocs/content/themes/brandcave/nav-main.php
+++ htdocs/content/themes/brandcave/nav-main.php
@@ -18,12 +18,17 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
               <ul class="nav navbar-nav navbar-right">
-                  <?php wp_list_pages('sort_column=menu_order&depth=2&title_li='); ?>
-                  <li><p>1 (800) 561-8790</p></li>
-               </ul>
-
+                <?php $args = array(
+                  'depth'        => 2,
+                  'exclude'      => '4',
+                  'post_status'  => 'publish',
+                  'sort_column'  => 'menu_order',
+                  'title_li'     => ''
+                ); ?>
+                <?php wp_list_pages($args); ?>
+                <li><p>1 (800) 561-8790</p></li>
+              </ul>
             </div><!-- /.navbar-collapse -->
           </div><!-- /.container-fluid -->
         </nav>
